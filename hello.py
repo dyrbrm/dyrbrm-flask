6357773ce2a45d5b4ce492f8c3ce0737b6097917
dyrbrm <steven.wong@imcpacific.com.au>
2015-10-28 15:14:25 +1100

    Updating hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "This is a test page!!"
 
 @app.route("/test/")
 def test():
-    return "Test page!"
+    return "Test environment"
+
+@app.route("/prod/")
+def prod():
+    return "Production environment"
+
+@app.route("/login")
+def login(): pass
+
+@app.route("/user/<username>")
+def profile(username): pass
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='Steven Wong')
 
 if __name__ == "__main__":
     app.debug = True
